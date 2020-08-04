158fabbcb0fca5091d6924d3f18438d4fcb0da19
WillCollier <61185593+WillCollier@users.noreply.github.com>
2020-08-04 20:22:16 +0100

    Add files via upload

--- coronavirus_deaths.py
+++ coronavirus_deaths.py
@@ -3,15 +3,87 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+from requests import get
+
+
+#---------Use government API to access data----------#
+
+def get_data(url):
+    response = get(endpoint, timeout=10)
+
+    if response.status_code >= 400:
+        raise RuntimeError(f'Request failed: {response.text}')
+
+    return response.json()
+
+
+if __name__ == '__main__':
+    endpoint = (
+        'https://api.coronavirus.data.gov.uk/v1/data?'
+        'filters=areaType=nation&'
+        'structure={"date":"date","newCases":"newCasesByPublishDate","cumCases":"cumCasesByPublishDate"'
+        ',"newDeaths":"newDeathsByPublishDate","cumDeaths":"cumDeathsByPublishDate","nation":"areaName"}'
+    )
+
+    data = get_data(endpoint)
+    print(data)
+
+def mergeDict(dict1, dict2):
+   ''' Merge dictionaries and keep values of common keys in list'''
+   dict3 = {**dict1, **dict2}
+   for key, value in dict3.items():
+       if key in dict1 and key in dict2:
+            # print(key, dict1[key], dict2[key], value)
+            if (type(value) == str )| (type(value) == int) | (type(value) == float):
+                dict3[key] = [value , dict1[key]]
+                # print("first", dict3[key])
+            elif type(value) == list:
+                value.append(dict1[key])
+                dict3[key] = value
+                # print("rest", dict3[key])
+   return dict3
+
+# Merge dictionaries and add values of common keys in a list
+for i in range(data['length']-1):
+    if i == 0:
+        dict = mergeDict(data["data"][i], data["data"][i+1])
+    else:
+        dict = mergeDict(data["data"][i+1], dict)
+
+cv_latest = pd.DataFrame(dict)
+
+#Seperate out the different countries
+#Reorder for cumulative in sensible direction
+England = cv_latest.loc[cv_latest['nation'] == 'England'].iloc[::-1]
+Wales = cv_latest.loc[cv_latest['nation'] == 'Wales'].iloc[::-1]
+Scotland = cv_latest.loc[cv_latest['nation'] == 'Scotland'].iloc[::-1]
+Northern_Ireland = cv_latest.loc[cv_latest['nation'] == 'Northern Ireland'].iloc[::-1]
+UK = cv_latest.loc[cv_latest['nation'] == 'United Kingdom'].iloc[::-1]
+
+#turn data into an actual usable quantity
+
+
+# if __name__ == '__main__':
+#     endpoint = (
+#         'https://api.coronavirus.data.gov.uk/v1/data?'
+#         'filters=areaType=region&'
+#         'structure={"date":"date","newCases":"newCasesByPublishDate","cumCases":"cumCasesByPublishDate"'
+#         ',"newDeaths":"newDeathsByPublishDate","cumDeaths":"cumDeathsByPublishDate"}'
+#     )
+#
+#     data = get_data(endpoint)
+#     print(data)
+
+
 #-------------------FUNCTIONS-----------------#
 def pullout_dcids(i, len_arr_out):
-    dcideaths = i['Daily change in deaths'].values
+    dcideaths = i['newDeaths'].values
     dci_deaths = np.pad(dcideaths, (len_arr_out - len(dcideaths), 0))
     dci_deaths[np.where(np.isnan(dci_deaths) == True)] = 0
     return dci_deaths
 
 def pullout_csums(i, len_arr_out):
-    cumdeaths = i['Cumulative deaths'].values
+    cumdeaths = i['cumDeaths'].values
     cum_deaths = np.pad(cumdeaths, (len(reporting_dates)-len(cumdeaths), 0))
     cum_deaths[np.where(np.isnan(cum_deaths)==True)] = 0
     return cum_deaths
@@ -32,23 +104,23 @@ def moving_avg(array_in, nOsteps):
 #-------------------Load dataset-----------------#
 #take dataset from
 # cv_deaths = pd.read_csv('../PhD/coronavirus-deaths_latest.csv')
-cv_deaths = pd.read_csv('../PhD/covid.csv')
-
-
-#Seperate out the different countries
-#Reorder for cumulative in sensible direction
-England = cv_deaths.loc[cv_deaths['Area name'] == 'England'].iloc[::-1]
-Wales = cv_deaths.loc[cv_deaths['Area name'] == 'Wales'].iloc[::-1]
-Scotland = cv_deaths.loc[cv_deaths['Area name'] == 'Scotland'].iloc[::-1]
-Northern_Ireland = cv_deaths.loc[cv_deaths['Area name'] == 'Northern Ireland'].iloc[::-1]
-UK = cv_deaths.loc[cv_deaths['Area name'] == 'United Kingdom'].iloc[::-1]
+# cv_deaths = pd.read_csv('../PhD/covid.csv')
+#
+#
+# #Seperate out the different countries
+# #Reorder for cumulative in sensible direction
+# England = cv_deaths.loc[cv_deaths['Area name'] == 'England'].iloc[::-1]
+# Wales = cv_deaths.loc[cv_deaths['Area name'] == 'Wales'].iloc[::-1]
+# Scotland = cv_deaths.loc[cv_deaths['Area name'] == 'Scotland'].iloc[::-1]
+# Northern_Ireland = cv_deaths.loc[cv_deaths['Area name'] == 'Northern Ireland'].iloc[::-1]
+# UK = cv_deaths.loc[cv_deaths['Area name'] == 'United Kingdom'].iloc[::-1]
 
 
 #combine into a single dataframe
 #create numpy arrays
-reporting_dates = UK['Reporting date'].values
+reporting_dates = England['date'].values
 len_array = reporting_dates.shape[0]
-for i in [England, Wales, Scotland, Northern_Ireland, UK]:
+for i in [England, Wales, Scotland, Northern_Ireland]:
 
     dci_deaths = pullout_dcids(i, len_array)
     rolling_avg = moving_avg(dci_deaths, 7)
@@ -60,7 +132,8 @@ for i in [England, Wales, Scotland, Northern_Ireland, UK]:
 
 
 
-Data = {'Date': reporting_dates[:,0], 'England_dcids': reporting_dates[:,1],
+Data = {
+        'Date': reporting_dates[:,0], 'England_dcids': reporting_dates[:,1],
         'England_rollavg': reporting_dates[:,2],
         'England_cum': reporting_dates[:,3], 'Wales_dcids': reporting_dates[:,4],
         'Wales_rollavg':reporting_dates[:,5],
@@ -68,8 +141,12 @@ Data = {'Date': reporting_dates[:,0], 'England_dcids': reporting_dates[:,1],
         'Scotland_rollavg':reporting_dates[:,8],
         'Scotland_cum': reporting_dates[:,9], 'Northern_Ireland_dcids': reporting_dates[:,10],
         'Northern_Ireland_rollavg':reporting_dates[:,11],
-        'Northern_Ireland_cum': reporting_dates[:,12], 'UK_dcids': reporting_dates[:,13],
-        'UK_rollavg':reporting_dates[:,14], 'UK_cum': reporting_dates[:,15]}
+        'Northern_Ireland_cum': reporting_dates[:,12],
+        'UK_dcids': reporting_dates[:,1]+reporting_dates[:,4]+reporting_dates[:,7]+reporting_dates[:,10],
+        'UK_rollavg': reporting_dates[:,2]+reporting_dates[:,5]+reporting_dates[:,8]+reporting_dates[:,11],
+        'UK_cum': reporting_dates[:,3]+reporting_dates[:,6]+reporting_dates[:,9]+reporting_dates[:,12]
+        }
+
 
 combined_df = pd.DataFrame(Data, columns=['Date', 'England_dcids', 'England_rollavg',
                                     'England_cum', 'Wales_dcids', 'Wales_rollavg',
